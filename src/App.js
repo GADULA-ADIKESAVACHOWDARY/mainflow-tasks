d8e32f3540b4e24b466beec2aa2da9877bf2be47
GADULA-ADIKESAVACHOWDARY <adikesavachowdary03@gmail.com>
2025-01-24 22:18:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+        <p>This is a basic React application developed in Visual Studio Code.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
